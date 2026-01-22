957ebcba98e3e45d40a31caab16d47f6d832b5de
trinath00a <123577880+trinath00a@users.noreply.github.com>
2026-01-22 22:54:24 +0530

    updated the app.js to verify the webhook
    Added a new line to the response message after the webhook change.

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from AWS CI/CD Pipeline!\n');
+  res.end('Hello from AWS CI/CD Pipeline!\n
+          'This is the new line added after the Webhook change updated');
 });
 
 server.listen(3000, () => {
