c26051339ced1c2a3dcdca30a94399b275b79799
trinath00a <123577880+trinath00a@users.noreply.github.com>
2026-01-22 22:56:27 +0530

    Update response message in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello from AWS CI/CD Pipeline!\n
-          'This is the new line added after the Webhook change updated');
+          'This is the new line added after the Webhook change updated, nko');
 });
 
 server.listen(3000, () => {
