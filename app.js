b656510ac387ef1182d560154a19bee5cf5d86fc
trinath00a <123577880+trinath00a@users.noreply.github.com>
2026-01-27 20:31:21 +0530

    Update response message in app.js display message was changed from Hello to Hii

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from AWS CI/CD Pipeline! This is the new line I want to for Webhook update.. updated the script with real time Blue-Green clusters!\n');
+  res.end('Hii from AWS CI/CD Pipeline! This is the new line I want to for Webhook update.. updated the script with real time Blue-Green clusters!\n');
 });
 
 server.listen(3000, () => {
